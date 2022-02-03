bb833dea4388d07a2d48a036cea34cb2ea9bd413
JamesRunnalls <james.runnalls@eawag.ch>
2022-02-03 17:48:43 +0100

    Add readme to PYPI

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+with open('README.md') as f:
+    README = f.read()
+
+VERSION = '0.0.3'
 DESCRIPTION = 'ENVASS ENVironmental data quality ASSurance.'
-LONG_DESCRIPTION = 'ENVironmental data quality ASSurance for generating high quality data products.'
 
 setup(
     name="envass",
     version=VERSION,
     author="James Runnalls",
-    author_email="<runnalls.james@gmail.com>",
+    author_email="<james.runnalls@eawag.ch>",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=README,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
+    url='https://github.com/eawag-surface-waters-research/envass',
     install_requires=[],
     license="MIT",
     keywords=['python', 'ENVASS', 'quality assurance', 'environmental data'],
