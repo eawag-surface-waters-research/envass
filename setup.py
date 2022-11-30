6c6b4791992bd4114b38826642803586b499d4c9
JamesRunnalls <james.runnalls@eawag.ch>
2022-11-30 10:41:24 +0100

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     README = f.read()
 
-VERSION = '0.0.6'
+VERSION = '0.1.0'
 DESCRIPTION = 'ENVASS ENVironmental data quality ASSurance.'
 
 setup(
@@ -17,12 +17,12 @@ setup(
     packages=find_packages(),
     url='https://github.com/eawag-surface-waters-research/envass',
     install_requires=[
-        'ipywidgets==7.6.5',
-        'numpy==1.19.5',
-        'pandas==1.1.5',
-        'plotly==5.6.0',
-        'scikit-learn==0.24.2',
-        'scipy==1.5.4'
+        'ipywidgets>=7.6.5',
+        'numpy>=1.19.5',
+        'pandas>=1.1.5',
+        'plotly>=5.6.0',
+        'scikit-learn>=0.24.2',
+        'scipy>=1.5.4'
     ],
     license="MIT",
     keywords=['python', 'ENVASS', 'quality assurance', 'environmental data'],
